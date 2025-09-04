16a2d6e33a018875739a7911ef3934241d3099d7
dmitry <yarashevich_dev@mail.ru>
2025-09-04 17:07:43 +0300

    changes for single-threaded mode

--- demo/src/main/java/com/example/demo/controller/MyEntityController.java
+++ demo/src/main/java/com/example/demo/controller/MyEntityController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.service.MyEntityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -17,6 +18,11 @@ public class MyEntityController {
     @PostMapping("/file")
     public ResponseEntity<String> addedFileInVersion(MultipartHttpServletRequest request) throws IOException {
         return service.addFileVersion(request);
+    }
+
+    @GetMapping("/cycle")
+    public ResponseEntity<String> callCycleFileInVersion() throws IOException {
+        return service.downloadAndAddMultipart();
     }
 }
 
--- demo/src/main/java/com/example/demo/service/MyEntityService.java
+++ demo/src/main/java/com/example/demo/service/MyEntityService.java
@@ -2,13 +2,19 @@ package com.example.demo.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.util.UriUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.management.BufferPoolMXBean;
 import java.lang.management.ManagementFactory;
@@ -17,18 +23,63 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class MyEntityService {
+
+    private static final String STORAGE_PATH = "/storage/tmp/";
+    private static final String POST_URL = "http://localhost:8088/file";
+
+    public ResponseEntity<String> downloadAndAddMultipart() {
+        for (int fileSize = 100; fileSize <= 800; fileSize += 100) {
+            byte[] fileBytes = generateFileContent(fileSize * 1024 * 1024);
+
+            for (int i = 0; i < 10; i++) {
+                downloadAndProcessFile(String.valueOf(fileSize), String.valueOf(i), fileBytes);
+            }
+        }
+        return ResponseEntity.ok("ok");
+    }
+
+    public void downloadAndProcessFile(String size, String iteration, byte[] fileBytes) {
+        System.out.println(size + " " + iteration);
+        RestClient restClient = RestClient.create();
+        try {
+            File tempFile = File.createTempFile(size + "_" + iteration + "_", ".zip");
+            Files.write(tempFile.toPath(), fileBytes);
+
+            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+            body.add("file", new FileSystemResource(tempFile));
+
+            restClient.post()
+                    .uri(POST_URL)
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .body(body)
+                    .retrieve()
+                    .toEntity(String.class);
+
+            tempFile.delete();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static byte[] generateFileContent(int sizeInBytes) {
+        byte[] fileContent = new byte[sizeInBytes];
+        ThreadLocalRandom.current().nextBytes(fileContent);
+        return fileContent;
+    }
+
     @Transactional
     public ResponseEntity<String> addFileVersion(MultipartHttpServletRequest request) throws IOException {
         try {
             log.info("Before reading : {} bytes", getDirectMemoryUsed());
             MultipartFile f = request.getFile("file");
             byte[] array = f.getBytes();
-            String fileName = "1111" + f.getOriginalFilename();
+            String fileName = STORAGE_PATH + f.getOriginalFilename();
             storeFile(array, fileName);
             log.info("After reading : {} bytes", getDirectMemoryUsed());
             return ResponseEntity.ok("ok");
@@ -40,7 +91,7 @@ public class MyEntityService {
     private void storeFile(byte[] array, String path) throws IOException {
         String decode = UriUtils.decode(path, StandardCharsets.UTF_8.toString());
         synchronized (this) {
-            Path savedPath = Paths.get("/storage/tmp", decode);
+            Path savedPath = Paths.get(decode);
             if (Files.notExists(savedPath.getParent()))
                 Files.createDirectories(savedPath.getParent());
             Files.write(savedPath, array);
